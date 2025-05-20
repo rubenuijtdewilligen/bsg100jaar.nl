00df4ef55ca7d948483e7bad191f72175a2eea58
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-05-20 02:28:04 +0200

    Update title and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,6 +20,7 @@
 </script>
 
 <svelte:head>
+  <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
@@ -33,6 +34,13 @@
     href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
     rel="stylesheet"
   />
+
+  <!-- Browser tab -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="/favicon/site.webmanifest" />
+  <title>{title}</title>
 </svelte:head>
 
 <Toaster />

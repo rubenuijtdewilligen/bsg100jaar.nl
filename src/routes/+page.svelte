634c6eb4b8d375eefb0d595cf8012a88e6a8d999
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-05-25 22:33:13 +0200

    Update homepage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,28 @@
-<h1 class="mb-4 text-center text-3xl font-bold">Lustrum XX: BSG 100 jaar!</h1>
+<h1 class="mb-4 text-center text-3xl font-bold">Waarde (aspirant-)(reünist-)Gildiaan,</h1>
+
+<p class="my-2">Welkom!</p>
 
 <p class="my-2">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec ultrices mi. Suspendisse nisi
-  est, imperdiet eu auctor eu, elementum sit amet felis. Donec dapibus mattis velit, ut venenatis
-  neque viverra nec. Vivamus pharetra pretium nisl eu condimentum. Nulla facilisi. Aenean et
-  interdum metus. Nam in pulvinar nisi, sed ultrices nisi. Vestibulum finibus, orci a sodales
-  dictum, arcu dui auctor mi, id pretium ipsum nisi et sem. Donec varius dictum porttitor. Phasellus
-  luctus porta orci, id placerat sapien cursus in. Mauris ultrices tincidunt suscipit. Nulla id
-  libero odio. Integer dictum pulvinar molestie.
+  De voorbereidingen voor ons 100-jarige bestaan zijn in volle gang, de lèmpkes worden ingedraaid.
+  De Lucie (de lustrumcommissie) is verheugd jullie te laten weten dat we op 6 juni 2026 in Oirschot
+  ons 20e lustrum gaan vieren!
+</p>
+<p class="my-2">
+  Deze mijlpaal van 100 jaar BSG laten we niet zomaar aan ons voorbijgaan en daarom zal er
+  voorafgaand aan het lustrum een lustrumjaar plaatsvinden. Gedurende dit jaar kan iedereen die dat
+  wil iets organiseren of maken ter ere van het lustrum. Met de creatieve geest waar menig BSG'er om
+  bekend staat, als het gaat om de invulling van activiteiten, kan dit niet anders dan een spektakel
+  worden! Het hoeft niet grootst te zijn. Samen buurten, herinneringen maken en proosten. Dat is het
+  belangrijkste.
 </p>
 
 <p class="my-2">
-  Morbi ligula urna, imperdiet at ultrices nec, commodo non magna. Aenean blandit ante ut
-  ullamcorper pulvinar. Vivamus odio felis, blandit eget rhoncus ac, iaculis sed eros. Morbi
-  vulputate tellus nec nisl scelerisque aliquet. Phasellus vel sodales tortor. Suspendisse massa
-  felis, accumsan et aliquet at, luctus vel ipsum. Suspendisse tincidunt vulputate enim, sit amet
-  facilisis mi. Proin fermentum semper velit vitae commodo. Vestibulum accumsan lacus nec sapien
-  vestibulum iaculis. Curabitur nec aliquet augue. In placerat molestie metus, et vestibulum nibh
-  tempor et. Duis volutpat, augue nec rutrum accumsan, urna lorem luctus nisi, id condimentum lorem
-  ante eget felis. Pellentesque faucibus lacus lorem, sed ornare nunc porttitor imperdiet. Lorem
-  ipsum dolor sit amet, consectetur adipiscing elit. Mauris id dui at orci faucibus lacinia sit amet
-  a augue.
+  Het lustrumjaar zal worden afgetrapt met de bekendmaking van ons motto op <b>5 juni 2025</b> in de
+  <b>Woeste Hoeve</b>
+  in <b>Wageningen</b>.
+</p>
+
+<p class="my-2">
+  Op deze website zal alle relevante informatie voor het lustrum komen te staan inclusief de
+  mogelijkheid om je aan te melden.
 </p>

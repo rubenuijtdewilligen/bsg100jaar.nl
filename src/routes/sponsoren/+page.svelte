df8e965934fe40280fdf8c7d0c2d29547b8b85d5
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-06-01 17:41:55 +0200

    Add sponsor page content

--- src/routes/sponsoren/+page.svelte
+++ src/routes/sponsoren/+page.svelte
@@ -1,24 +1,9 @@
 <h1 class="mb-4 text-center text-3xl font-bold">Sponsoren</h1>
 
 <p class="my-2">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec ultrices mi. Suspendisse nisi
-  est, imperdiet eu auctor eu, elementum sit amet felis. Donec dapibus mattis velit, ut venenatis
-  neque viverra nec. Vivamus pharetra pretium nisl eu condimentum. Nulla facilisi. Aenean et
-  interdum metus. Nam in pulvinar nisi, sed ultrices nisi. Vestibulum finibus, orci a sodales
-  dictum, arcu dui auctor mi, id pretium ipsum nisi et sem. Donec varius dictum porttitor. Phasellus
-  luctus porta orci, id placerat sapien cursus in. Mauris ultrices tincidunt suscipit. Nulla id
-  libero odio. Integer dictum pulvinar molestie.
-</p>
-
-<p class="my-2">
-  Morbi ligula urna, imperdiet at ultrices nec, commodo non magna. Aenean blandit ante ut
-  ullamcorper pulvinar. Vivamus odio felis, blandit eget rhoncus ac, iaculis sed eros. Morbi
-  vulputate tellus nec nisl scelerisque aliquet. Phasellus vel sodales tortor. Suspendisse massa
-  felis, accumsan et aliquet at, luctus vel ipsum. Suspendisse tincidunt vulputate enim, sit amet
-  facilisis mi. Proin fermentum semper velit vitae commodo. Vestibulum accumsan lacus nec sapien
-  vestibulum iaculis. Curabitur nec aliquet augue. In placerat molestie metus, et vestibulum nibh
-  tempor et. Duis volutpat, augue nec rutrum accumsan, urna lorem luctus nisi, id condimentum lorem
-  ante eget felis. Pellentesque faucibus lacus lorem, sed ornare nunc porttitor imperdiet. Lorem
-  ipsum dolor sit amet, consectetur adipiscing elit. Mauris id dui at orci faucibus lacinia sit amet
-  a augue.
+  Voor het lustrum zijn wij nog opzoek naar sponsoren! Heb je interesse om het lustrum financieel te
+  ondersteunen en je bedrijfsnaam hieraan te verbinden? Of zou je graag vriend willen worden van het
+  lustrum? Laat dan vooral je contactgegevens achter via het volgende formulier:
+  <a href="https://ap.lc/qKRIx" class="link link-primary" target="_blank">https://ap.lc/qKRIx</a>,
+  dan komen wij zo snel mogelijk bij je terug.
 </p>

9fe85bb4e40cad98c608430955648f1fe1fd5499
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2025-06-07 19:55:53 +0200

    Update sponsor form link

--- src/routes/sponsoren/+page.svelte
+++ src/routes/sponsoren/+page.svelte
@@ -4,6 +4,7 @@
   Voor het lustrum zijn wij nog opzoek naar sponsoren! Heb je interesse om het lustrum financieel te
   ondersteunen en je bedrijfsnaam hieraan te verbinden? Of zou je graag vriend willen worden van het
   lustrum? Laat dan vooral je contactgegevens achter via het volgende formulier:
-  <a href="https://ap.lc/qKRIx" class="link link-primary" target="_blank">https://ap.lc/qKRIx</a>,
-  dan komen wij zo snel mogelijk bij je terug.
+  <a href="https://forms.office.com/e/UMvuQUcEjx " class="link link-primary" target="_blank"
+    >https://forms.office.com/e/UMvuQUcEjx
+  </a>, dan komen wij zo snel mogelijk bij je terug.
 </p>

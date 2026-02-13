853800d20b419c965dd541769a972c5b37f4189a
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2026-02-13 19:19:15 +0100

    Update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,107 @@
-<h1 class="mb-4 text-center text-3xl font-bold">Waarde (aspirant-)(reünist-)Gildiaan,</h1>
+<div class="mb-4 flex flex-col items-center">
+  <a
+    href="https://events.flextickets.nl/event/bsg-100-jaar"
+    class="btn btn-primary btn-lg text-white">TICKETS!</a
+  >
+</div>
 
-<p class="my-2">Welkom!</p>
+<h1 class="text-center text-3xl font-bold">Het feest van de eeuw: 100 jaar BSG!</h1>
 
 <p class="my-2">
-  De voorbereidingen voor ons 100-jarige bestaan zijn in volle gang, de lèmpkes worden ingedraaid.
-  De Lucie (de lustrumcommissie) is verheugd jullie te laten weten dat we op <b>6 juni 2026</b> in
-  <b>Oirschot</b>
-  ons 20e lustrum gaan vieren!
-</p>
-<p class="my-2">
-  Deze mijlpaal van 100 jaar BSG laten we niet zomaar aan ons voorbijgaan en daarom zal er
-  voorafgaand aan het lustrum een lustrumjaar plaatsvinden. Gedurende dit jaar kan iedereen die dat
-  wil iets organiseren of maken ter ere van het lustrum. Met de creatieve geest waar menig BSG'er om
-  bekend staat, als het gaat om de invulling van activiteiten, kan dit niet anders dan een spektakel
-  worden! Het hoeft niet groots te zijn. Samen buurten, herinneringen maken en proosten. Dat is het
-  belangrijkste.
+  Op <b>6 juni 2026</b> vieren we samen met leden, oud-leden en reünisten
+  <b>100 jaar Brabants Studenten Gilde</b>. Een volle dag herinneringen ophalen en nieuwe
+  herinneringen maken.
 </p>
 
 <p class="my-2">
-  Het lustrumjaar zal worden afgetrapt met de bekendmaking van ons motto op <b>5 juni 2025</b> in de
-  <b>Woeste Hoeve</b>
-  in <b>Wageningen</b>.
+  Zorg dat JIJ erbij bent in Oirschot, zodat ook jij kunt zeggen: <b>Hier heb ik meer gestaan!</b>
 </p>
 
-<p class="my-2">
-  Op deze website zal alle relevante informatie voor het lustrum komen te staan inclusief de
-  mogelijkheid om je aan te melden.
+<h3 class="mt-3 text-xl font-bold underline">Het programma</h3>
+<h3 class="text-lg font-bold">10:30 - Officiële opening</h3>
+<p class="mb-3">
+  We starten in de <b>Kerk van Oirschot</b> met een feestelijke opening, met onder andere
+  gedeputeerde Dhr van Gruijthuijsen van Provincie Brabant. Journalist <b>Paul Spapens</b> neemt ons
+  op levendige wijze mee naar de vroege jaren van BSG, waarin tijdens de Paaskampen in diverse Brabantse
+  dorpen sociografische onderzoeken uitgevoerd zijn. De opening is voor iedereen toegankelijk.
 </p>
+
+<h3 class="text-lg font-bold">12.30 - Naar de Beurs & lunch</h3>
+<p>
+  Met alle BSG leden en reünisten verplaatsen we ons naar <b>De Beurs</b> (op 100 meter), onze uitvalsbasis
+  voor de rest van de dag.
+</p>
+<p class="mb-3">We beginnen met een <b>goed verzorgde lunch</b>: hét moment om bij te praten.</p>
+
+<h3 class="text-lg font-bold">14.00 uur – Middagactiviteiten</h3>
+<p class="mb-3">
+  De middag bestaat uit twee delen, verveling is uitgesloten. Voor ieder wat wils: van <b
+    >rikken tot een stadswandeling</b
+  >. Natuurlijk ontbreekt ook het <b>Ruure en Pruuve</b> niet.
+</p>
+<h3 class="text-lg font-bold">17.00 uur – Borrel & diner</h3>
+<p class="mb-3">We luiden de avond in met een gezellige borrel en heerlijk diner.</p>
+<h3 class="text-lg font-bold">20.00 – 01.30 uur – Feest!</h3>
+<p class="mb-3">
+  Het feest barst los in de feestzaal van De Beurs met <b>coverband Hotsho</b> en een <b>DJ</b>. De
+  voetjes-van-de-vloer-cie kan weer shinen!
+</p>
+
+<h3 class="text-xl font-bold underline">Blijven slapen?</h3>
+Geen zin om naar huis te rijden? Geen probleem!
+
+<ul class="mb-3 list-inside list-disc">
+  <li>
+    <b>Gratis slaapmogelijkheid</b> voor studenten en recent afgestudeerde reünisten op 3 km afstand
+    (geef dit aan bij opgave).
+  </li>
+  <li><b>Tentplek</b> op 3 km afstand (geef dit aan bij opgave).</li>
+  <li>
+    <b>Camper- of caravanplek</b> op 3 km afstand (max 10 plekken beschikbaar) (geef dit aan bij opgave).
+  </li>
+  <li>
+    <b>Camperplaats Oirschot</b> (2 km van De Beurs) als alternatief <br />
+    <a href="https://www.camperplaatsoirschot.nl/" class="link"
+      >https://www.camperplaatsoirschot.nl/</a
+    ><br />
+    <a href="tel:+310616243738" class="link">+31 (0)6 16243738</a>
+  </li>
+  <li>
+    <b>Hotels:</b>
+    <ul class="ml-8 list-inside list-disc">
+      <li>
+        <b>Hotel 46 – Wintelre</b> (13 km) → gebruik kortingscode “VIP” <br />
+        <a href="https://www.hotel46.nl/" class="link">https://www.hotel46.nl/</a>
+      </li>
+      <li>
+        <b>Van der Valk Hotel Eindhoven-Best</b> (12 km) <br />
+        <a href="https://www.valkeindhovenbest.nl/" class="link"
+          >https://www.valkeindhovenbest.nl/</a
+        >
+      </li>
+    </ul>
+  </li>
+</ul>
+
+<h3 class="text-lg font-bold">Zondag 7 juni – 10.00 uur</h3>
+<p class="mb-3">
+  Ontbijt bij De Beurs: het perfecte moment om samen na te genieten, herinneringen op te halen en
+  alvast nieuwe afspraken te maken met degenen met wie je nog niet genoeg bent bijgebuurt.
+</p>
+
+<h2 class="text-xl font-bold underline">
+  PROOST op 100 jaar BSG: Daar moet (vaker) op gedronken worden!
+</h2>
+Omdat 100 jaar BSG vaker gevierd mag worden, zijn drie reünisten en één lid in januari de<b
+  >dorpsbrouwerij van Terheijden</b
+>
+ingedoken. Het resultaat: een exclusief <b>donker bier (±7,5%)</b>, speciaal gebrouwen voor dit
+jubileum. Je kunt dit bier kopen bij je opgave. De flessen (<b>75 cl</b>) ontvang je op
+<b>6 juni</b>, zodat je later thuis nog eens kunt proosten op <b>100 jaar BSG</b>.
+
+<div class="mt-4 flex flex-col items-center">
+  <a
+    href="https://events.flextickets.nl/event/bsg-100-jaar"
+    class="btn btn-primary btn-lg text-white">TICKETS!</a
+  >
+</div>
